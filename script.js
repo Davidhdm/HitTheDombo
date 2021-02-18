b7dd867c5f4d8025a6ccae19b2eaa23bf366749a
David <david.hdm9@gmail.com>
2021-02-18 11:53:10 +0100

    marcianito 100% real no feic

--- script.js
+++ script.js
@@ -8,3 +8,44 @@ function changelogin(){
         btnLogin.innerHTML = "Log in";
     }
 }
+
+function audio1(){
+    let drum1 = document.getElementById("drum1");
+    
+    if (drum1.paused){
+        drum1.play();
+    }else{
+        drum1.pause();
+    }
+}
+
+function audio2(){
+    let drum1 = document.getElementById("drum2");
+    
+    if (drum1.paused){
+        drum1.play();
+    }else{
+        drum1.pause();
+    }
+}
+
+
+function audio3(){
+    let drum1 = document.getElementById("drum3");
+    
+    if (drum1.paused){
+        drum1.play();
+    }else{
+        drum1.pause();
+    }
+}
+
+function audio4(){
+    let drum1 = document.getElementById("drum4");
+    
+    if (drum1.paused){
+        drum1.play();
+    }else{
+        drum1.pause();
+    }
+}
